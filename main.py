c566e5fb9e42f600210f2e6198cd68be98e6e939
Vivek Mishra <vivekmishra.vk97@gmail.com>
2021-07-18 15:54:27 +0000

    15-07-2021 commit

--- main.py
+++ main.py
@@ -7,10 +7,13 @@
 from __future__ import print_function, division
 import realtime_monitoring
 import torch
+import rapl
 from memory_profiler import profile
 import io
 import pprofile
-import cProfile, pstats, sys
+import cProfile
+import pstats
+import sys
 import pyRAPL
 import psutil
 import pandas as pd
@@ -24,29 +27,28 @@ from torchvision import datasets, models, transforms
 import matplotlib.pyplot as plt
 import time
 import os
+from torch.nn.parallel import DistributedDataParallel as DDP
+import torch.multiprocessing as mp
+import torch.distributed as dist
 import copy
 
-# def imshow(inp, title=None):
-#     """Imshow for Tensor."""
-#     inp = inp.numpy().transpose((1, 2, 0))
-#     mean = np.array([0.485, 0.456, 0.406])
-#     std = np.array([0.229, 0.224, 0.225])
-#     inp = std * inp + mean
-#     inp = np.clip(inp, 0, 1)
-#     plt.imshow(inp)
-#     if title is not None:
-#         plt.title(title)
-#     plt.pause(0.001)  # pause a bit so that plots are updated
-
-#pyRAPL.setup()
+# pyRAPL.setup()
 
 #csv_output = pyRAPL.outputs.CSVOutput('energy_pyRAPL.csv')
 
-#@pyRAPL.measure(output=csv_output)
-@profile(precision=3)
+# @pyRAPL.measure(output=csv_output)
+# @profile(precision=3)
 def train_model(model, criterion, optimizer, scheduler, num_epochs=1):
     since = time.time()
-
+    # -------------------------------------------------------------------
+    #rank = 3
+    # dist.init_process_group(
+    #	backend='gloo',
+    #		init_method='env://',
+    #	world_size=4,
+    #	rank=rank
+    # )
+    # -------------------------------------------------------------------
     best_model_wts = copy.deepcopy(model.state_dict())
     best_acc = 0.0
 
@@ -113,38 +115,35 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=1):
     model.load_state_dict(best_model_wts)
     return model
 
-# def visualize_model(model, num_images=6):
-#     was_training = model.training
-#     model.eval()
-#     images_so_far = 0
-#     fig = plt.figure()
-#
-#     with torch.no_grad():
-#         for i, (inputs, labels) in enumerate(dataloaders['val']):
-#             inputs = inputs.to(device)
-#             labels = labels.to(device)
-#
-#             outputs = model(inputs)
-#             _, preds = torch.max(outputs, 1)
-#
-#             for j in range(inputs.size()[0]):
-#                 images_so_far += 1
-#                 ax = plt.subplot(num_images//2, 2, images_so_far)
-#                 ax.axis('off')
-#                 ax.set_title('predicted: {}'.format(class_names[preds[j]]))
-#                 # imshow(inputs.cpu().data[j])
-#
-#                 if images_so_far == num_images:
-#                     model.train(mode=was_training)
-#                     return
-#         model.train(mode=was_training)
 
 if __name__ == '__main__':
 
     plt.ion()   # interactive mode
 
+    # ------------------------
+    #os.environ['MASTER_ADDR'] = '10.57.23.164'
+    #os.environ['MASTER_PORT'] = '8888'
+
+    # create default process group
+    #dist.init_process_group("gloo", rank=3, world_size=4)
+    #rank = 3
+    # dist.init_process_group(
+    #    backend='gloo',
+    #            init_method='env://',
+    #    world_size=4,
+    #    rank=rank
+    # )
+    # -----------------------
+
     zip = zipfile.ZipFile('hymenoptera_data.zip')
     zip.extractall()
+
+    torch.set_num_threads(4)
+    torch.set_num_interop_threads(80)
+    num_thred = torch.get_num_threads()
+    num_interop_thred = torch.get_num_interop_threads()
+    print("Num Threads:",num_thred," ",num_interop_thred)
+
 
     # Data augmentation and normalization for training
     # Just normalization for validation
@@ -167,14 +166,22 @@ if __name__ == '__main__':
     image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x),
                                               data_transforms[x])
                       for x in ['train', 'val']}
+
+    # ------------------------------------------------------------
+    # train_sampler = torch.utils.data.distributed.DistributedSampler(
+    #	image_datasets,
+    #    num_replicas=4,
+    #	rank=3)
+    # ------------------------------------------------------------
+
     dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=4,
-                                                 shuffle=True, num_workers=4)
-                  for x in ['train', 'val']}
+                                                  shuffle=True, num_workers=4)  # ,pin_memory=True,
+                   # sampler=train_sampler)
+                   for x in ['train', 'val']}
     dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
     class_names = image_datasets['train'].classes
 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
 
     # Get a batch of training data
     inputs, classes = next(iter(dataloaders['train']))
@@ -183,8 +190,6 @@ if __name__ == '__main__':
     out = torchvision.utils.make_grid(inputs)
 
     # imshow(out, title=[class_names[x] for x in classes])
-
-
 
     # model_ft = models.resnet18(pretrained=True)
     # torch.save(model_ft, "resnet18.pt")
@@ -196,14 +201,19 @@ if __name__ == '__main__':
 
     model_ft = model_ft.to(device)
 
+    # -------------------------------------------
+    # model_ft = nn.parallel.DistributedDataParallel(model_ft,
+    #                                            device_ids=None)
+    # -------------------------------------------
+
     criterion = nn.CrossEntropyLoss()
 
     # Observe that all parameters are being optimized
     optimizer_ft = optim.SGD(model_ft.parameters(), lr=0.001, momentum=0.9)
 
     # Decay LR by a factor of 0.1 every 7 epochs
-    exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
-
+    exp_lr_scheduler = lr_scheduler.StepLR(
+        optimizer_ft, step_size=7, gamma=0.1)
 
     # gives a single float value
     cpu_per_b = psutil.cpu_percent()
@@ -214,45 +224,64 @@ if __name__ == '__main__':
     # you can have the percentage of used RAM
     vir_mem_per_b = psutil.virtual_memory().percent
     # you can calculate percentage of available memory
-    mem_av_per_b = psutil.virtual_memory().available * 100 / psutil.virtual_memory().total
+    mem_av_per_b = psutil.virtual_memory().available * 100 / \
+        psutil.virtual_memory().total
 
-
+    num_epochs = 1
+    #torch.set_num_threads(4)
+    #torch.set_num_interop_threads(80)
+    #num_thred = torch.get_num_threads()
+    #num_interop_thred = torch.get_num_interop_threads()
+    #print("Num Threads:",num_thred," ",num_interop_thred)
     profiler = pprofile.Profile()
     pr = cProfile.Profile()
+    #s1 = rapl.RAPLMonitor.sample()
     pr.enable()
     start_train = time.time()
     with profiler:
         model_ft = train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler,
-                           num_epochs=1)
-        #csv_output.save()
+                               num_epochs=1)
+        # mp.spawn(train_model, nprocs=4, args=(criterion,optimizer_ft,exp_lr_scheduler,
+        #    num_epochs))
+        # csv_output.save()
     end_train = time.time()
     pr.disable()
+    #s2 = rapl.RAPLMonitor.sample()
+    #diff = s2 - s1
+    #print(diff)
+    # Print the difference in microjoules
+    #print("CPU Energy Usage(microjoules): ",diff.energy("package-0", "core", rapl.UJOULES))
+
+    # Print the average power
+    #print("Average Power(CPU Usage): ",diff.average_power("package-0", "core"))
+   # for d in diff.domains:
+   #     print(d)
+   #     domain = diff.domains[d]
+   #     power = diff.average_power(package=domain.name)
+   #     print("%s - %0.2f W" % (domain.name, power))
+
+   #     for sd in domain.subdomains:
+   #         subdomain = domain.subdomains[sd]
+   #         power = diff.average_power(
+   #             package=domain.name, domain=subdomain.name)
+   #         print("\t%s - %0.2f W" % (subdomain.name, power))
     elapsed_time = end_train-start_train
     with open('Elapsed_time.txt', 'w') as f1:
         f1.write("Training Time(Elapsed):")
         f1.write(str(elapsed_time))
     f1.close()
     # gives a single float value
-    # profiler.print_stats()
-    # Or to a file:
     profiler.dump_stats("exec_time.txt")
     cpu_per_a = psutil.cpu_percent()
     # gives an object with many fields
-    # vir_mem_a = psutil.virtual_memory()
     # you can convert that object to a dictionary
     vir_mem_a = dict(psutil.virtual_memory()._asdict())
     # you can have the percentage of used RAM
     vir_mem_per_a = psutil.virtual_memory().percent
     # you can calculate percentage of available memory
-    mem_av_per_a = psutil.virtual_memory().available * 100 / psutil.virtual_memory().total
-
-    # ps.print_stats()
-
-    # CPU_table = [["CPU Usage Percent", cpu_per_b, cpu_per_a], ["Memory Used", vir_mem_b, vir_mem_a],
-    #          ["Memory Used(%)", vir_mem_per_b, vir_mem_per_a], ["Memory Available(%)", mem_av_per_b, mem_av_per_a]]
-    # print(tabulate(CPU_table, headers=["Metric", "Before Training", "After Training"], tablefmt="pretty"))
+    mem_av_per_a = psutil.virtual_memory().available * 100 / \
+        psutil.virtual_memory().total
     with open('CPU_table.txt', 'w') as f:
-        # f.write(tabulate(CPU_table, headers=["Metric", "Before Training", "After Training"], tablefmt="pretty"))
         f.write("BEFORE TRAINING:--------\n")
         f.write("CPU USAGE(%):")
         f.write(str(cpu_per_b))
@@ -287,25 +316,11 @@ if __name__ == '__main__':
     result = result.getvalue()
     # chop the string into a csv-like buffer
     result = 'ncalls' + result.split('ncalls')[-1]
-    result = '\n'.join([','.join(line.rstrip().split(None, 5)) for line in result.split('\n')])
+    result = '\n'.join([','.join(line.rstrip().split(None, 5))
+                       for line in result.split('\n')])
     # save it to disk
 
     with open('memory_logs.csv', 'w+') as f:
         # f=open(result.rsplit('.')[0]+'.csv','w')
         f.write(result)
         f.close()
-
-    # # ps = pstats.Stats(pr, stream=sys.stdout)
-    # s = io.StringIO()
-    # ps = pstats.Stats(pr, stream=s).sort_stats('tottime')
-    # # ps.print_stats()
-    #
-    # with open('memory_logs.txt', 'w+') as f:
-    #     f.write(s.getvalue())
-    # f.close()
-
-    # data_cpu = pd.read_csv("energy_pyRAPL.csv")
-    # Preview the first 5 lines of the loaded data
-    # data_cpu.head()
-
-    # visualize_model(model_ft)
--- realtime_monitoring.py
+++ realtime_monitoring.py
@@ -44,8 +44,11 @@ if __name__ == "__main__":
     mem_use = []
     dict_cpu_c = {}
     dict_mem_c = {}
+    dict_track_pro = {}
+    time_clk = -4
     try:
         while 1:
+            time_clk = time_clk+4
             # get all process info
             processes = get_processes_info()
             os.system("cls") if "nt" in os.name else os.system("clear")
@@ -55,7 +58,7 @@ if __name__ == "__main__":
             #try:
             s = df[df.eq('mprof').any(1)]
             if s.empty:
-                print('DataFrame is empty!')
+                print('Script Not Running: Start the Script')
             else:
                 #print("pid:   ",s.iloc[0]['pid'])
                 #print(s)
@@ -106,7 +109,9 @@ if __name__ == "__main__":
                     pid_t_c_i = str(pid_t_c)
                     dict_cpu_c.setdefault(pid_t_c_i, [])
                     dict_mem_c.setdefault(pid_t_c_i, [])
+                    dict_track_pro.setdefault(pid_t_c_i, [])
                     dict_cpu_c[pid_t_c_i].append(child_process.iloc[0]['cpu_usage'])
+                    dict_track_pro[pid_t_c_i].append(time_clk)
                     dict_mem_c[pid_t_c_i].append(child_process.iloc[0]['memory_usage'])
                 cpu_use.append(cpu_childs)
                 mem_use.append(mem_childs)
@@ -121,9 +126,9 @@ if __name__ == "__main__":
         len_cnt = len(cpu_use)
         len_cnt = len_cnt*4
         x_time = list(range(0, len_cnt, 4))
-        print("Timestamps: ",x_time)
-        print("CPU Consumption(%): ",cpu_use)
-        print("Memory Usages: ",mem_use)
+        #print("Timestamps: ",x_time)
+        #print("CPU Consumption(%): ",cpu_use)
+        #print("Memory Usages: ",mem_use)
         plt.plot(x_time, cpu_use, '-o')
         plt.xlabel('Time(in secs)')
         plt.ylabel('CPU Consumption(%)')
@@ -135,5 +140,30 @@ if __name__ == "__main__":
         plt.ylabel('Memory Consumption(MB)')
         plt.title('Memory usage using second process - All Child Processes combined')
         plt.savefig('memory_consumption_with_secondPRO.png')
-
+        
+        plt.clf()
+        procs_tot = dict_track_pro.keys()
+        num_procs_tot = len(procs_tot)
+        for i in procs_tot:
+            x_child_p = dict_track_pro[i]
+            y_cpu_p = dict_cpu_c[i]
+            plt.plot(x_child_p, y_cpu_p, '-o', label = i)
+            plt.xlabel('Time(in secs)')
+            plt.ylabel('CPU Consumption(%)')
+            plt.title('CPU usage using second process - Every Process')
+        plt.legend()
+        plt.savefig('cpu_consumption_every_process.png')
+        plt.clf()
+        procs_tot = dict_track_pro.keys()
+        num_procs_tot = len(procs_tot)
+        for i in procs_tot:
+            x_child_p = dict_track_pro[i]
+            y_mem_p = dict_mem_c[i]
+            plt.plot(x_child_p, y_mem_p, '-o', label = i)
+            plt.xlabel('Time(in secs)')
+            plt.ylabel('Memory Consumption(MB)')
+            plt.title('Memory(MB) Using second pro-Every Process')
+        plt.legend()
+        plt.savefig('memory_consumption_every_pro.png')
+        plt.clf()
 
